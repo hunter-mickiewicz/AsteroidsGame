f8281a8b3684ce0c3db39b15be48b3c8b294feff
hunter.mickiewicz <hunter.mickiewicz@gmail.com>
2022-10-11 16:22:01 -0600

    Asteroids are now randomly generating resources

--- Assets/Scripts/Asteroids/AsteroidProperties.cs
+++ Assets/Scripts/Asteroids/AsteroidProperties.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AsteroidProperties : MonoBehaviour
 {
-    public Dictionary<string, int> elements = new Dictionary<string, int>();
+    public Dictionary<string, double> elements = new Dictionary<string, double>();
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +17,9 @@ public class AsteroidProperties : MonoBehaviour
         */
         //Instantiate random number generator
         System.Random gen = new System.Random();
-        int tier = gen.Next(0,100);// (Start, stop)
-        int relMass = 100 + gen.Next(1,10);
+        //int tier = gen.Next(0,100);// (Start, stop)
+        int tier = 99;
+        double relMass = 100 + gen.Next(1,10);
 
         //calculates which tier the asteroid is in, falls through to calculate elements
         //takes relative mass, calculates the existence of minerals, subtracts the relative mass
@@ -45,34 +46,37 @@ public class AsteroidProperties : MonoBehaviour
                 break;
         }
 
-        foreach (KeyValuePair<string, int> entry in elements)
+        //Uncomment to print out the values generated
+        /*
+        foreach (KeyValuePair<string, double> entry in elements)
         {
             Debug.Log(entry);
         }
+        */
 
-        //Calculate mass dependent on tier and number of elements
+        //Todo: Calculate mass dependent on tier and number of elements
     }
 
-    void calculateDarkMatter(int rel, System.Random ran) {
+    void calculateDarkMatter(double rel, System.Random ran) {
         //takes 1/5 of the possible mass to work with
-        int workingMass = rel / 5;
+        double workingMass = rel / 10;
 
         //calculates what percentage to work with, between 1/2 -> 1
         double percMass = ran.Next(5, 10);
-        workingMass *= (int)(percMass / 10);
+        workingMass *= percMass / 10;
         elements["DarkMatter"] = workingMass;
         rel -= workingMass;
-        Debug.Log("Tier iv: relMass = " + rel);
+
         calculatePlats(rel, ran);
     }
 
-    void calculatePlats(int rel, System.Random ran) {
+    void calculatePlats(double rel, System.Random ran) {
         //takes 1/5 of the possible mass to work with
-        int workingMass = rel / 3;
+        double workingMass = rel / 3;
 
         //calculates what percentage to work with, between 1/2 -> 1
         double percMass = ran.Next(5, 10);
-        workingMass *= (int)(percMass / 10);
+        workingMass *= percMass / 10;
 
         string[] elementArray = {"Ruthenium", "Rhodium", "Palladium", "Osmium", "Iridium", "Platinum"};
 
@@ -80,46 +84,44 @@ public class AsteroidProperties : MonoBehaviour
         foreach (string element in elementArray)
         {
             //base of 1/5, can reach as high as 1/7
-            int frac = 5;
+            double frac = 5;
             frac += ran.Next(0, 2);
 
             //takes a random chunk out of working mass, assigns in dictionary
-            int elemMass = workingMass / frac;
+            double elemMass = workingMass / frac;
             rel -= elemMass;
             elements[element] = elemMass;
         }
 
-        Debug.Log("Tier iii: relMass = " + rel);
         calculateIndustrials(rel, ran);
     }
 
-    void calculateIndustrials(int rel, System.Random ran)
+    void calculateIndustrials(double rel, System.Random ran)
     {
         //takes 1/5 of the possible mass to work with
-        int workingMass = rel / 2;
+        double workingMass = rel / 2;
 
         //calculates what percentage to work with, between 1/2 -> 1
         double percMass = ran.Next(5, 10);
-        workingMass *= (int)(percMass / 10);
+        workingMass *= percMass / 10;
         string[] elementArray = { "Iron", "Cobalt", "Nickel" };
 
         foreach (string element in elementArray)
         {
             //base of 1/2, can reach as high as 1/4
-            int frac = 2;
+            double frac = 2;
             frac += ran.Next(0, 2);
 
             //takes a random chunk out of working mass, assigns in dictionary
-            int elemMass = workingMass / frac;
+            double elemMass = workingMass / frac;
             rel -= elemMass;
             elements[element] = elemMass;
         }
 
-        Debug.Log("Tier ii: relMass = " + rel);
         calculateVolatiles(rel, ran);
     }
 
-    void calculateVolatiles(int rel, System.Random ran)
+    void calculateVolatiles(double rel, System.Random ran)
     {
         //rel is whatever remains after the others took their chunks out
 
@@ -127,17 +129,14 @@ public class AsteroidProperties : MonoBehaviour
         foreach(string element in elementArray)
         {
             //doing 7/24, + or - 1/24 (halfway between 1/3 and 1/24
-            Debug.Log(rel);
-            int frac = 7;
+            double frac = 7;
             frac += ran.Next(-1, 1);
-            
-            int elemMass = (int)(rel * (double)(frac / 24));
-            Debug.Log(elemMass);
+
+            double elemMass = rel * frac / 24;
             rel -= elemMass;
             elements[element] = elemMass;
         }
 
-        Debug.Log("Tier i: relMass = " + rel);
     }
 
     // Update is called once per frame
