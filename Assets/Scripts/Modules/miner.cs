6a3d499ca2ec005c9335b01ca26bcec1898569b1
hunter.mickiewicz <hunter.mickiewicz@gmail.com>
2022-10-17 17:09:10 -0600

    light testing, commenting framework for mining mod

---
+++ Assets/Scripts/Modules/miner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class miner : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //IDEA FOR CLASS
+    //mod added to attached rigidbody allows the attached thing to mine asteroid (within a certain range, start with minimal range)
+    //  rudimentary test shows we likely need to be <0.6 distance
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
